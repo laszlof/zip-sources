ffadf509d6072d7f2f852f6849cef8ed4080970e
Frank Laszlo <flaszlo@nexcess.net>
2019-02-11 09:22:16 -0500

    updates

--- nex-bc/nex-bc.php
+++ nex-bc/nex-bc.php
@@ -1,4 +1,7 @@
 <?php
+/*
+ Plugin Name: Nexcess BigCommerce OAuth Connector
+ */
 
 add_filter('bigcommerce/oauth_connector/url', function () {
     return 'https://us-central1-orbital-bee-231121.cloudfunctions.net/nex-bc-auth-connector/v1';
