fccfbed81db0f0f7eee7a69aa6847bf5c3edeff3
Frank Laszlo <flaszlo@nexcess.net>
2019-02-12 11:58:56 -0500

    fixes

--- nex-bc-oauth-connector/nex-bc-oauth-connector.php
+++ nex-bc-oauth-connector/nex-bc-oauth-connector.php
@@ -3,8 +3,6 @@
  Plugin Name: Nexcess BigCommerce OAuth Connector
  */
 
-if (empty(get_option('bigcommerce_client_secret', ''))) {
-  add_filter('bigcommerce/oauth_connector/url', function () {
-    return 'https://us-central1-orbital-bee-231121.cloudfunctions.net/nex-bc-auth-connector/v1';
-  });
-}
+add_filter('bigcommerce/oauth_connector/url', function() {
+  return 'https://us-central1-orbital-bee-231121.cloudfunctions.net/nex-bc-auth-connector/v1';
+});
